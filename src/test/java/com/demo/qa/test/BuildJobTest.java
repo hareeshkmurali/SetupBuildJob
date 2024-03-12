8c5a6714ef159aab8c71299931fea4d987a5f887
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2024-03-12 12:41:32 +0530

    Update BuildJobTest.java

--- src/test/java/com/demo/qa/test/BuildJobTest.java
+++ src/test/java/com/demo/qa/test/BuildJobTest.java
@@ -1,37 +1,41 @@
+package com.demo.qa.test;
+
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;  // Import ChromeOptions
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class BuildJobTest {
-    WebDriver driver;
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setUp()
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); //Implicit wait
+		driver.get("https://demoqa.com/");
+	}
+	
+	@Test
+	public void ToolsQaTest()
+	{
+		driver.findElement(By.xpath("(//div[@class=\"card-up\"])[1]")).click();
+		String expectedtext="Elements";
+		String actualtext=driver.findElement(By.xpath("(//div[contains(text(),\"Elements\")])[1]")).getText();
+		Assert.assertEquals(expectedtext, actualtext, "Message is not Displaying as expected");
+	}
+	
+	@AfterMethod
+	public void tearDown()
+	{
+		driver.close();
+	}
 
-    @BeforeMethod
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-
-        // Create ChromeOptions instance
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");  // Add the --headless argument
-
-        // Initialize ChromeDriver with options
-        driver = new ChromeDriver(options);
-
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // Implicit wait
-        driver.get("https://demoqa.com/");
-    }
-
-    // ... rest of your test methods ...
-
-    @AfterMethod
-    public void tearDown() {
-        driver.quit(); // Using quit() to close all browser windows
-    }
 }
